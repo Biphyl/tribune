f3badc1c7b673a825eca45e4356e876284f6e9da
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-27 08:35:17 +0300

    add welcome note

--- news/views.py
+++ news/views.py
@@ -15,8 +15,11 @@ if request.method == 'POST':
         if form.is_valid():
             name = form.cleaned_data['your_name']
             email = form.cleaned_data['email']
+
             recipient = NewsLetterRecipients(name = name,email =email)
             recipient.save()
+            send_welcome_email(name,email)
+            
             HttpResponseRedirect('news_today')
     else:
         form = NewsLetterForm()
